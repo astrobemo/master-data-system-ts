fbc089f3bf4f5d40ecb14c36df8b00540c942f22
astrobemo <astrobemo@gmail.com>
2025-05-09 19:09:32 +0700

    updated the audited pckaagejson and tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from 'express';
+import { setPool, getPool } from './database/poolManager.js';
 
 const app = express();
-
 // Middleware
 app.use(express.json());
 
+(async () => {  
+    try {
+      // Initialize the default pool
+      await setPool();
+      console.log('Default database pool initialized successfully.');
+    } catch (error) {
+      console.error('Failed to initialize database pool:', error);
+      process.exit(1); // Exit the process if the pool cannot be initialized
+    }
+})();
+
 // Routes
 app.get('/', (req, res) => {
-	res.send('Hello, World!');
+	res.send('Master Data System API is running!');
 });
 
 export default app;
--- src/database/poolManager.ts
+++ src/database/poolManager.ts
@@ -10,9 +10,9 @@ let pool: Pool | null = null;
  */
 export const setPool = (config?: object): void => {
   pool = new Pool({
-    user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432', 10),
     ...config, // Allow overriding default config
